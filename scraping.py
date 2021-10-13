89c608fc32ea2d1443a1b8c1077c52b05868ade4
Gao-Xixi <77395018+Gao-Xixi@users.noreply.github.com>
2021-10-13 14:46:53 -0400

    web scraping done

--- scraping.py
+++ scraping.py
@@ -13,11 +13,24 @@ class CourseRecord:
         print(f'Title: {self.title}')
         print(f'Detail: {self.detail}')
 # according to subject name, return the subjectPopUp used for post method
-def getsubjectPopUp(subject):
+def geturl():
+    r = requests.get("https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm")
+    bs = BeautifulSoup(r.text, 'lxml')
+    ul = bs.find('ul',class_='bodytext')
+    href = ul.li.a['href']
+    href = f"https://w2prod.sis.yorku.ca/{href}"
+    print(href)
+    r = requests.get(href)
+    bs = BeautifulSoup(r.text, 'lxml')
+    form = bs.find('form', {'method': 'post'})
+    url = form['action']
+    print(url)
+    return url
+
+def getsubjectPopUp(url,subject):
     # mainpage
-    rs = requests.get("https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm")
-    
-    r = requests.get("https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm.woa/6/wo/TtYg7b0FwLJVOhXWKPjDDw/15.3.10.7")
+    r = requests.get(url)
+    wosid = url.split('/')[-2]
     bs = BeautifulSoup(r.text, 'lxml')
     options = bs.find_all('option')
     for option in options:
@@ -25,66 +38,71 @@ def getsubjectPopUp(subject):
             subjectPopUp = int(option['value'])
             print(subjectPopUp)
             return subjectPopUp
-    # raise Exception('Wrong major name')
-def getpage(subject):
-    subjectPopUp = getsubjectPopUp(subject)
+    return None
+
+def getpage(url,subject):
+    subjectPopUp = getsubjectPopUp(url,subject)
+    wosid = url.split('/')[-2]
     params={
     'sessionPopUp': 0,
     # 0-207 subjects, 64 is EECS
     'subjectPopUp': subjectPopUp,
     '3.10.7.5': 'Search Courses',
-    'wosid': 'TtYg7b0FwLJVOhXWKPjDDw'
+    'wosid': wosid
     }
-    req = requests.post("https://w2prod.sis.yorku.ca/Apps/WebObjects/cdm.woa/6/wo/TtYg7b0FwLJVOhXWKPjDDw/15.3.10.7",
-                      data=params)
+    req = requests.post(url,data=params)
 
     soup = BeautifulSoup(req.text,'lxml')
+    print(wosid)
     return soup
-def scrapeSubject(subject):
-    soup = getpage(subject)
+def scrapeSubject(url,subject):
+    soup = getpage(url,subject)
     sub = soup.find('td',{'width':'16%'}).text
     sub = sub.split(" ")[0]
+    print(sub)
     return sub
-def scrapeCourses(subject):
+def scrapeCourses(url,subject):
     numbers = []
     credits = []
-    soup = getpage(subject)
+    soup = getpage(url,subject)
     courses = soup.find_all('td',{'width':'16%'})
     for course in courses:
         number = course.text.split(" ")[1]
         credit = course.text.split(" ")[-1]
         numbers.append(number)
         credits.append(credit)
-        # print (f"{number} + {credit}")
+    print(numbers)
+    print(credits)
     return numbers,credits
-def scrapeTitles(subject):
+def scrapeTitles(url,subject):
     result = []
-    soup = getpage(subject)
+    soup = getpage(url,subject)
     titles = soup.find_all('td', {'width': '24%'})
     for title in titles:
         title = title.text
         result.append(title)
-
+    print(result)
     return result
     # return titles_
-def scrapeDetails(subject):
-    details_ = []
-    soup = getpage(subject)
+def scrapeDetails(url,subject):
+    result = []
+    soup = getpage(url,subject)
     details = soup.find_all('td', {'width': '30%'})
     for detail in details:
         if detail.text:
             href = detail.a['href']
             href = f"https://w2prod.sis.yorku.ca/{href}"
-            details_.append(href)
-    return details_
+            result.append(href)
+    print(result)
+    return result
     # if details is None:
 
-def getCourseRecords(subject):
+def getCourseRecords(url,subject):
     records = []
-    subject_ = scrapeSubject(subject)
-    numbers, credits = scrapeCourses(subject)
-    titles = scrapeTitles(subject)
-    details = scrapeDetails(subject)
+    subject_ = scrapeSubject(url,subject)
+    numbers, credits = scrapeCourses(url,subject)
+    titles = scrapeTitles(url,subject)
+    details = scrapeDetails(url,subject)
     for i in range(len(numbers)):
         course_record = CourseRecord(subject_, numbers[i],credits[i], titles[i],details[i])
         records.append(course_record)
@@ -93,7 +111,17 @@ def getCourseRecords(subject):
 if __name__ == "__main__":
     # records = getCourseRecords()
     subject = input("int put your major(for example EECS): ")
-    try:
-        getsubjectPopUp(subject)
-    except Exception:
-        print("Cannot find your major!")
+    # try:
+    url = geturl()
+    # getsubjectPopUp(url, subject)
+    # getpage(url, subject)
+    # scrapeSubject(url, subject)
+    # scrapeCourses(url, subject)
+    # scrapeTitles(url, subject)
+    scrapeDetails(url, subject)
+    getCourseRecords(url, subject)
+    # getCourseRecords(url,subject)
+    # except Exception:
+    #     print("Cannot find your major!")
+
+
